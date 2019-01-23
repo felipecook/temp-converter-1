6e263f5dab9bb9baa96c8bce07def61167fea2ae
McSmallpox <ryanwands18@gmail.com>
2019-01-23 13:48:05 -0700

    Added generated Java source

---
+++ src/JavaApplication.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean repeat;
+        
+        do {
+            double fahrenheit;
+            double celsius;
+            
+            fahrenheit = input.nextDouble();
+            celsius = (fahrenheit - 32) * 5 / 9;
+            System.out.println(celsius);
+            System.out.println("Would you like to calculate again? true or false");
+            repeat = input.nextBoolean();
+        } while (repeat);
+    }
+}
